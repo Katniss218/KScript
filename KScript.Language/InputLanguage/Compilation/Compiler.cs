c57154083f82a1608b474fceabef4b3f52783b80
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2022-06-22 20:31:23 +0200

    Began work on the high level compiler class

---
+++ KScript.Language/InputLanguage/Compilation/Compiler.cs
@@ -0,0 +1,67 @@
+﻿using KScript.Language.Parsing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KScript.Language.InputLanguage.Compilation
+{
+    /// <summary>
+    /// A high level class intended to be used by the end user.
+    /// </summary>
+    public class Compiler
+    {
+        const string KSCRIPT_FILE_EXTENSION = ".ks";
+        /// <summary>
+        /// Directory with the source (.ks) files.
+        /// </summary>
+        public string InputDirectory { get; set; }
+
+        /// <summary>
+        /// Directory for the output files.
+        /// </summary>
+        public string OutputDirectory { get; set; }
+
+        private Lexer _lexer;
+        private Parser _parser;
+
+        public Compiler( string inputDirectory, string outputDirectory )
+        {
+            this.InputDirectory = inputDirectory;
+            this.OutputDirectory = outputDirectory;
+        }
+
+        public void CompileAll()
+        {
+            // TODO - clear output.
+
+            string[] sourceFiles = Directory.GetFiles( InputDirectory, $"*{KSCRIPT_FILE_EXTENSION}" );
+
+            foreach( var sourcePath in sourceFiles )
+            {
+                // TODO - exception handling
+
+                string source = File.ReadAllText( sourcePath, Encoding.UTF8 );
+
+                string compiled = Compile( source );
+
+                string relativePath = sourcePath.Replace( InputDirectory, "" );
+
+                string outputPath = Path.Combine( OutputDirectory, relativePath );
+
+                File.WriteAllText( outputPath, compiled, Encoding.UTF8 );
+            }
+        }
+
+        // TODO - How do we handle compiler warnings/errors? (can't be exceptions to the end user).
+
+        private string Compile( string source )
+        {
+            // Performs the actual compilation of the source file.
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- KScript.Language/OutputLanguage/CodeGenerator/CodeGen.cs
+++ KScript.Language/OutputLanguage/CodeGenerator/CodeGen.cs
@@ -8,6 +8,18 @@ namespace KScript.Language.OutputLanguage.CodeGenerator
 {
     public class CodeGen
     {
+        // first you feed the compiler the script tiles (text files).
+
+        // The compiler runs them through the lexer and the parser and produces a CST.
+
+        // The code generator then uses the CST to produce a list of opcodes and their arguments, and save them into a file.
+
+
+        //-=-=-=-=-=-=-=-
+
+        // Thought points:
+
+        // We don't have to assign integer values to the opcodes. They can be abstracted away in the runtime. The output file can be more akin to an assembler file, and the runtime then uses that.
 
     }
 }
